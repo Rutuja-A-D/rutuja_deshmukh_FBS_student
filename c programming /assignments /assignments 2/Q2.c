f2b50fb6e124189d82d20a3fd332c2eea08a91cf
Rutuja-A-D <rutujadeshmukh794@gmail.com>
2025-06-16 03:10:44 -0700

    Add files via upload

---
+++ c programming /assignments /assignments 2/Q2.c
@@ -0,0 +1,12 @@
+void main()
+{
+	int s1=10,s2=10,s3=10;
+	if(s1==s2&&s2==s3)
+	printf("this is an equilatrl triangle");
+	else if(s1==s2||s2==s3)
+	printf("this is an isoscales triangle");
+	else
+	printf("this is a scalene triangle");
+	
+	
+	}
