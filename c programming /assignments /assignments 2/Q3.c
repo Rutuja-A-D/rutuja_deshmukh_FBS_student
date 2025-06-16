c4f365c76aae1f0bfc194e4927b9616b07203014
Rutuja-A-D <rutujadeshmukh794@gmail.com>
2025-06-16 03:11:41 -0700

    Add files via upload

---
+++ c programming /assignments /assignments 2/Q3.c
@@ -0,0 +1,20 @@
+//Write a program to find greatest of three numbers using nested if-else.
+void main()
+{
+	int a=-12,b=-2,c=10;
+	if(a>b)
+	{
+	 if(a>c)
+	 printf("a is greter");
+	 else
+	printf ("c is greater");
+	 	}
+	 else if (b>c)
+	 {
+	 printf("b is greater");
+      }
+	 else
+	{
+	  printf("c is greater");
+	 	 }
+}
